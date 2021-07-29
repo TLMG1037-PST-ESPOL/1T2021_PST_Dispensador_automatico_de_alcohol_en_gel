57d7b31e55e623fa5dfda1fdb2bf8737d2aa559a
ceormaza <ceormaza@espol.edu.ec>
2021-07-29 18:52:04 -0500

    get

--- backend/src/controllers/usuariosControl.js
+++ backend/src/controllers/usuariosControl.js
@@ -1,5 +1,6 @@
 const userControl={};
 const db=require("./database.js")
+const userControl={}
 
 userControl.getUsuarios=(req,res)=>{
     db.query("Select * FROM usuario", (err,result,fields)=>{
@@ -10,6 +11,22 @@ userControl.getUsuarios=(req,res)=>{
         }
         res.json(result);
     });
+}
+
+// obtener informacion de la DB
+userControl.getUsuario=(req, res)=>{
+    if(isNaN(req.params.id)){
+        res.status(400).send("Id no valido");
+        return;
+    }
+    db.query("SELECT * FROM usuario WHERE id="+req.params.id, (err, result, fields)=>{
+        if(err){
+            res.status(500).send(err);
+            console.log(err);
+            return;
+        }
+        res.json(result)
+    })
 }
 
 module.exports=userControl;
