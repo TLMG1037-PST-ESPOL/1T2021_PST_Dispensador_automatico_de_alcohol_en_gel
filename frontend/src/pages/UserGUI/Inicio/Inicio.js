31709d80cee70dd70e10ccf8cb792d68615eef17
vicemora <vicemora@espol.edu.ec>
2021-07-30 16:18:06 -0500

    commit  2 avance final

--- frontend/src/pages/UserGUI/Inicio/Inicio.js
+++ frontend/src/pages/UserGUI/Inicio/Inicio.js
@@ -8,12 +8,13 @@ import Cookies from 'universal-cookie';
 const cookies= new Cookies() 
 
 export default class Inicio extends Component {
+
     render() {
         console.log(cookies.get('correo_electronico'));
         console.log(cookies.get('nombre_usuario'));
         return (
             <div>
-                <TaskBar/>
+                <TaskBar text= "Menu Principal" />
             </div>
         )
     }
--- frontend/src/pages/UserGUI/Inicio/components/TaskBar/TaskBar.js
+++ frontend/src/pages/UserGUI/Inicio/components/TaskBar/TaskBar.js
@@ -1,14 +1,25 @@
 import React from "react";
+import { Link } from 'react-router-dom'
 
-
-const TaskBar = () => {
+const TaskBar = ({text}) => {
   const mobile = window.innerWidth < 600;
 
   return (
     <div>
-
-      <label className>  </label>
-
+    <nav className="navbar navbar-expand-sm navbar-dark bg-dark .text-dark">
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/inicio">{text}</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav">
+            <Link className="nav-link active" aria-current="page" to="/dispositivo">Dispositivos</Link>
+            <Link className="nav-link" to="/">Log Out</Link>
+            </div>
+        </div>
+      </div>
+    </nav>
     </div>
   )}
 export default TaskBar
