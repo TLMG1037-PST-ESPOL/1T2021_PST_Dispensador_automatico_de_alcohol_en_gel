778fd2a49784a24decf45c24cb1d80fd24d5847f
ceormaza <ceormaza@espol.edu.ec>
2021-07-30 15:17:56 -0500

    carolina

--- frontend/src/pages/UserGUI/Inicio/Inicio.js
+++ frontend/src/pages/UserGUI/Inicio/Inicio.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react'
-/*import Cookies from 'universal-cookie';
+import Cookies from 'universal-cookie';
 
-const cookies= new Cookies() */
+const cookies= new Cookies() 
 
 export default class Inicio extends Component {
     render() {
-        //console.log(cookies.get('usuario:'+nombre_usuario));
+        console.log(cookies.get('correo_electronico'));
+        console.log(cookies.get('nombre_usuario'));
         return (
             <div>
                 Inicio de sesion exitoso
