60718e5cb7764a99c09bad72ee8b113637933bd3
vicemora <vicemora@espol.edu.ec>
2021-07-30 14:46:17 -0500

    asda

--- frontend/src/pages/Register/Registro.js
+++ frontend/src/pages/Register/Registro.js
@@ -25,55 +25,61 @@ class Registro extends Component{
     console.log(this.state.users);
   } 
 
-    //Funciones onChange para manejar evento del input
-    onChangeEmail = (e)=>{
-      this.setState({
+  //Funciones onChange para manejar evento del input
+  onChangeEmail = (e)=>{
+    this.setState({
           userEmail: e.target.value
-      })
-      console.log(this.state.userEmail)
+    })
+    console.log(this.state.userEmail)
     }
     
-    onChangeUser = (e)=>{
-      this.setState({
-          userName: e.target.value
-      })
-    }
-    
-    onChangePassword = (e)=>{
-      this.setState({
-          userPassword: e.target.value
-      })
-    }
+  onChangeUser = (e)=>{
+    this.setState({
+        userName: e.target.value
+    })
+  }
   
-    //Manejador para boton
+  onChangePassword = (e)=>{
+    this.setState({
+        userPassword: e.target.value
+    })
+  }
 
-    onClick = async (e)=>{
-      e.preventDefault();
-      try {
+  //Manejador para boton
+
+  onClick = async (e)=>{
+    e.preventDefault();
+    try {
+        
+        const res = await axios.post(backend.host + ':' + backend.port + '/usuarios',
+          { id_rol_usuario: 1,
+            nombre_usuario:this.state.userName,
+            correo_electronico:this.state.userEmail,
+            contraseña:this.state.userPassword
+          })
+        console.log(res);
+        if(res){
+          alert("Cuenta registrada")
           
-          const res = await axios.post(backend.host + ':' + backend.port + '/usuarios',
-            { id_rol_usuario: 1,
-              nombre_usuario:this.state.userName,
-              correo_electronico:this.state.userEmail,
-              contraseña:this.state.userPassword
-            })
-          console.log(res);
-      } catch (error) {
-          alert("Datos incompletos o usuario ya existe"); //personalizar errores para lanzarlos y manejarlos
-      }
-      
-      this.getUsuarios();
-      this.setState({userName:''});
-      this.setState({userEmail:''});
-      this.setState({userPassword:''});
-      
+        }
+    } catch (error) {
+        alert("Datos incompletos o usuario ya existe"); //personalizar errores para lanzarlos y manejarlos
     }
-    //metodo render
+    
+    this.getUsuarios();
+    this.setState({userName:''});
+    this.setState({userEmail:''});
+    this.setState({userPassword:''});
+    
+  }
+  
+  //metodo render
   render(){
     return(
       <div className="containerRegistro">
       <div className="containerRegistrosSec">
           <div className="form-group2">
+          <Link className='nav-link active' to="/"><label> Atras </label></Link>
           <label>Correo electrónico: </label>
           <input
             type="text"
@@ -105,9 +111,11 @@ class Registro extends Component{
             
             
           />
+           
           <button className="Registrarse" onClick={this.onClick}>Registrarse
           </button>
           
+         
 
         </div>
       </div>
