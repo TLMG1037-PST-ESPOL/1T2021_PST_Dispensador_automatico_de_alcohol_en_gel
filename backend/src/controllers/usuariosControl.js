d2d9ddc4bb8cac0930750d7952c29b4d40f5420a
ceormaza <ceormaza@espol.edu.ec>
2021-07-29 20:24:10 -0500

    carolina

--- backend/src/controllers/usuariosControl.js
+++ backend/src/controllers/usuariosControl.js
@@ -1,6 +1,5 @@
 const userControl={};
 const db=require("./database.js")
-const userControl={}
 
 userControl.getUsuarios=(req,res)=>{
     db.query("Select * FROM usuario", (err,result,fields)=>{
@@ -19,7 +18,7 @@ userControl.getUsuario=(req, res)=>{
         res.status(400).send("Id no valido");
         return;
     }
-    db.query("SELECT * FROM usuario WHERE id="+req.params.id, (err, result, fields)=>{
+    db.query("SELECT * FROM usuario WHERE id_usuario="+req.params.id, (err, result, fields)=>{
         if(err){
             res.status(500).send(err);
             console.log(err);
@@ -29,4 +28,40 @@ userControl.getUsuario=(req, res)=>{
     })
 }
 
+//agregar informacion a la DB
+userControl.postUsuario=(req,res)=>{
+    const {id_usuario, id_rol_usuario, nombre_usuario, correo_electronico, contraseña}=req.body;
+    if(!id_rol_usuario || !nombre_usuario || !correo_electronico || !contraseña){
+        res.status(400).send("Datos incompletos");
+        return;
+    }
+    let SQLbody={}
+    if (!id_usuario){
+        SQLbody={id_rol_usuario, nombre_usuario, correo_electronico, contraseña};
+    }else{
+        SQLbody={id_usuario, id_rol_usuario, nombre_usuario, correo_electronico, contraseña};
+    }
+    db.query("INSERT INTO usuario SET ?",[SQLbody],
+    (err,result)=>{
+        if(err){
+            res.status(500).send(err);
+            console.log(err);
+            return;
+        }
+        res.send("Usuario ingresado")
+    });
+}
+
+//eliminar inf de la DB
+userControl.deleteUsuario=(req,res)=>{
+    db.query("DELETE FROM usuario WHERE id_usuario="+req.params.id,
+    (err, result)=>{
+        if(err){
+            res.status(500).send(err);
+            console.log(err);
+            return;
+        }
+        res.send("Usuario eliminado")
+    })
+}
 module.exports=userControl;
