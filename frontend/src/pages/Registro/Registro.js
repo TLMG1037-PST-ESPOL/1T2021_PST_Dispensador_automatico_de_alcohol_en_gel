6424de0a8f00f0648f22151f6ffabf50d2e02328
vicemora <vicemora@espol.edu.ec>
2021-07-30 00:19:17 -0500

    k

--- frontend/src/pages/Registro/Registro.js
+++ frontend/src/pages/Registro/Registro.js
@@ -4,10 +4,32 @@ import React from 'react'
 const Registro=()=>{
 
     return(
-        <div>
-           Hola mundo             
+        <div className="containerPrincipal">
+        <div className="containerSecundario">
+            <text Registrarse />
+            <div className="form-group">
+            <label>Usuario: </label>
+            <br />
+            <input
+              type="text"
+              className="form-control"
+              name="username"
+              
+            />
+            <br />
+            <label>Contraseña: </label>
+            <br />
+            <input
+              type="password"
+              className="form-control"
+              name="password"
+             
+            />
+            <br />
+            <button className="btn btn-primary" onClick={()=> this.iniciarSesion()}>Iniciar Sesión</button>
+          </div>
         </div>
-    )
-
+      </div>
+        )
 }
 export default Registro
