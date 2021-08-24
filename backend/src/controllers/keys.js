46273acff016dbcc31e98edcc23abcdb7f41caf6
Jerry Landivar <jlandiva@espol.edu.ec>
2021-08-24 02:59:04 -0500

    Codigo ESP32

--- backend/src/controllers/keys.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    database: {
-        host: 'sql10.freesqldatabase.com',
-        user: 'sql10432175',
-        password: 'uHfKBfXaIM',
-        database: 'sql10432175'
-    }
-};
-
